7b69daec810ebf856ae5bce21262a4fd605f0172
RockThe420 <33629969+RockThe420@users.noreply.github.com>
2020-04-02 18:01:36 +0300

    InfiniteScroll
    Lmao painis

--- FinanceApp/App.js
+++ FinanceApp/App.js
@@ -1,19 +1,28 @@
+// Imports: Dependencies
+import {decode, encode} from 'base-64'
+if (!global.btoa) {  global.btoa = encode }
+if (!global.atob) { global.atob = decode }
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import * as firebase from 'firebase';
+import 'firebase/firestore';
+import firebaseConfig from './config';
+// Imports: Screens
+import InfiniteScroll from './screens/InfiniteScroll';
+// Firebase: Initialize
+firebase.initializeApp({
+  apiKey: firebaseConfig.apiKey,
+  authDomain: firebaseConfig.authDomain,
+  databaseURL: firebaseConfig.databaseURL,
+  projectId: firebaseConfig.projectId,
+  storageBucket: firebaseConfig.storageBucket,
+  messagingSenderId: firebaseConfig.messagingSenderId,
+});
+// Firebase: Cloud Firestore
+const database = firebase.firestore();
+// React Native: App
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    
+    <InfiniteScroll database={database}/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
