9fe05ed04e183571b656fac6914fc2859baf027c
Partikas123 <martynaspartiks@gmai.com>
2020-04-02 16:40:27 +0300

    beleka
    belekas

---
+++ FinanceApp/item-show/Items.js
@@ -0,0 +1,25 @@
+Import test1 from '../';
+
+
+<View style={styles.container}>
+       <View style={styles.item}
+           <Image
+              source={test1}
+             style={styles.itemPhoto}
+           />
+           />
+          <Text           style={styles.itemText}
+           lorem gtx  
+          />,
+          <Text           style={styles.itemText}
+           lorem bla bla bla bla
+          />,
+          <Text           style={styles.itemText}
+           lorem bla bla bla bla
+          />
+       </View>
+</View>
+
+const styles = StyleSheet.create({
+
+})
