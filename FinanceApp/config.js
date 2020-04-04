a7b1f9a402365d00459a6bc32bf2c953dd564ed4
RockThe420 <33629969+RockThe420@users.noreply.github.com>
2020-04-04 18:40:28 +0300

    Ugniui
    Viskas

---
+++ FinanceApp/config.js
@@ -0,0 +1,13 @@
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDDnMP8LMS7YvkGelmhFqs50pA-xyHH1-E",
+    authDomain: "project-6b6fa.firebaseapp.com",
+    databaseURL: "https://project-6b6fa.firebaseio.com",
+    projectId: "project-6b6fa",
+    storageBucket: "project-6b6fa.appspot.com",
+    messagingSenderId: "856988943340",
+    appId: "1:856988943340:web:d6a8355b6ebb39701736c9",
+    measurementId: "G-KSXXZCZ773"
+  };
+  //Export
+  module.exports = firebaseConfig;
